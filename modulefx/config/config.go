7d8b84f3d5e47eba2d8074a729d1b2320f035ce9
nghiatn <nghiatn@vng.com.vn>
2023-06-21 11:37:46 +0700

    + init

---
+++ modulefx/config/config.go
@@ -0,0 +1,4 @@
+package config
+
+// for GIT revision: https://stackoverflow.com/questions/28459102/golang-compile-environment-variable-into-binary
+// for inject config values for running: https://gobyexample.com/environment-variables
